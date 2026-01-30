0c910e7f68a0a5a3dd9f021feb3b235b04212536
Lucas Araujo Pereira Matos <132773123+Lucas-pro-tech@users.noreply.github.com>
2026-01-30 09:31:49 -0300

    Updates

--- Comparador-De-Precos/script.js
+++ Comparador-De-Precos/script.js
@@ -1,10 +1,13 @@
 const searchForm = document.querySelector('.search-form');
+const productList = document.querySelector('.product-list');
 
 searchForm.addEventListener('submit', async (event) => {
   event.preventDefault();
 
   const inputValue = event.target[0].value.trim();
   if (!inputValue) return;
+
+  productList.innerHTML = `<p>🔎 Buscando produtos...</p>`;
 
   const url = `https://dummyjson.com/products/search?q=${encodeURIComponent(inputValue)}`;
 
@@ -13,8 +16,21 @@ searchForm.addEventListener('submit', async (event) => {
     const data = await response.json();
     const products = data.products.slice(0, 10);
 
-    console.log(products);
+    displayItems(products);
   } catch (error) {
     console.error("Erro:", error);
+    productList.innerHTML = `<p>❌ Erro ao buscar produtos.</p>`;
   }
 });
+
+function displayItems(products) {
+  productList.innerHTML = products
+    .map(product => `
+      <div class="product-card">
+        <img src="${product.images[0]}" alt="${product.title}" loading="lazy">
+        <h3>${product.title}</h3>
+        <p>R$ ${product.price}</p>
+      </div>
+    `)
+    .join('');
+}
